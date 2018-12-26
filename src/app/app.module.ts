99c0f63ccef90ef7118757e8fe454f40b79fe1c9
Nikola Guzina <nikolaguzina.ns@gmail.com>
2018-12-26 10:25:05 +0100

    Adding all files for new project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserComponent } from './users-list/user/user.component';
+import { UserEditComponent } from './users-list/user-edit/user-edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavmenuComponent,
+    UsersListComponent,
+    UserComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule
